7a94d56fddc1e1e72f582d6670cfa0eeb62c08fa
Kevin Burke <kev@inburke.com>
2016-10-25 14:18:13 -0700

    Upgrade twilio-go to 0.28
    Support for the IncomingPhoneNumber resource.

--- assets/bindata.go
+++ assets/bindata.go
@@ -101,7 +101,7 @@ func templatesBaseHtml() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "templates/base.html", size: 4628, mode: os.FileMode(420), modTime: time.Unix(1477424710, 0)}
+	info := bindataFileInfo{name: "templates/base.html", size: 4628, mode: os.FileMode(420), modTime: time.Unix(1477425087, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
