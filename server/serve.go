fb043f3db720b2508ef746d6a764cefaac3236b1
Kevin Burke <kev@inburke.com>
2016-10-26 12:18:01 -0700

    0.42

--- server/serve.go
+++ server/serve.go
@@ -23,7 +23,7 @@ import (
 )
 
 // Server version, run "make release" to increase this value
-const Version = "0.41"
+const Version = "0.42"
 
 func authUserHandler(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
