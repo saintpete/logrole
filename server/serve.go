d7f6dc81194db73d4d912709a6efd11f7a7b65f3
Kevin Burke <kev@inburke.com>
2016-11-10 21:18:48 -0800

    1.0

--- server/serve.go
+++ server/serve.go
@@ -25,7 +25,7 @@ import (
 )
 
 // Server version, run "make release" to increase this value
-const Version = "0.77"
+const Version = "1.0"
 
 func getRemoteIP(r *http.Request) string {
 	fwd := r.Header.Get("X-Forwarded-For")
